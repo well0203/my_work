a3674fdd59c397f530cc7be87cb18471fe2ac1d0
well0203 <scientist900lvl@gmail.com>
2024-09-27 02:10:23 +0200

    finalized Loss_function scripts!

--- PatchTST-main/PatchTST_supervised/data_provider/data_factory.py
+++ PatchTST-main/PatchTST_supervised/data_provider/data_factory.py
@@ -1,11 +1,7 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred
+from data_provider.data_loader import Dataset_Custom, Dataset_Pred
 from torch.utils.data import DataLoader
 
 data_dict = {
-    'ETTh1': Dataset_ETT_hour,
-    'ETTh2': Dataset_ETT_hour,
-    'ETTm1': Dataset_ETT_minute,
-    'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
 }
 
