533cd9cc7fe719084ce298a9c539e59e1e21b5cf
well0203 <scientist900lvl@gmail.com>
2024-10-21 21:34:55 +0200

    finish script with patchtst experiments

--- PatchTST-main/PatchTST_supervised/utils/tools.py
+++ PatchTST-main/PatchTST_supervised/utils/tools.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import time
+import os
 
 plt.switch_backend('agg')
 
@@ -66,6 +67,8 @@ class EarlyStopping:
     def save_checkpoint(self, val_loss, model, path):
         if self.verbose:
             print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        if not os.path.exists(path):
+            os.makedirs(path)
         torch.save(model.state_dict(), path + '/' + 'checkpoint.pth')
         self.val_loss_min = val_loss
 
